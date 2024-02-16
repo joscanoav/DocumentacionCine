de2b3df774a5822ea0300e203d8a4bead46ef1c8
Jorge Oscanoa <joscanoav@gmail.com>
2024-02-16 12:38:37 +0100

    refactor(se reformula el main por defecto): se agrega codigo para que pueda correr el programa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pelicula pelicula1 = new Pelicula("Titanic",180,10.0,"18:00");
+        Pelicula pelicula2 = new Pelicula("Matrix",150,12.0,"20:30");
+
+        Butaca[][] butacasSala1 = new Butaca[5][10];
+        Butaca[][] butacasSala2 = new Butaca[5][10];
+
+
+        // Recorrer cada butaca y crean nuevo objeto butaca para inicializar
+        for(int i= 0; i < 5; i++) {
+            for(int j=0;j<10;j++) {
+                butacasSala1[i][j] = new Butaca();
+            }
         }
+
+        for( int i=0; i < 5; i++) {
+            for(int j=0; j<10; j++) {
+                butacasSala2[i][j]=new Butaca();
+            }
+        }
+
+        Sala sala1 = new Sala(1,pelicula1,butacasSala1);
+        Sala sala2 = new Sala(2, pelicula2, butacasSala2);
+
+        // Se crea array llamado cine para crear dos salas
+
+        Sala[] salas = {sala1, sala2};
+
+        Cine cine = new Cine("Cine Planet",50, salas,50,0.0);
+        //Realizar operaciones en el cine
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\n=== Menú Principal ===");
+            System.out.println("1. Visualizar información del cine");
+            System.out.println("2. Realizar compra de entradas");
+            System.out.println("3. Consultar disponibilidad");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese la opción deseada: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    cine.visualizarInformacion();
+                    break;
+                case 2:
+                    cine.realizarCompra();
+                    break;
+                case 3:
+                    cine.consultarDisponibilidad();
+                    break;
+                case 4:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtelo de nuevo.");
+                    break;
+            }
+        } while (opcion != 4);
     }
 }
